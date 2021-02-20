1146174e9d1d7fc06d8f9693dbbc903315df2410
Greenbirch2007 <291109028@qq.com>
2021-02-20 13:20:15 +0800

    2.20 chg

---
+++ 吸取教训做更加详细的笔记(2021.2.20)/d.py
@@ -0,0 +1,10 @@
+import os
+
+themes =("$sys$","$proc$","$dev$","modprobe","lsmod","lspci","lsusb","dmesg","journalctl","BIOS","UEFI","bootloader","kernel","initramfs","init","SysVinit","systemd","$etc$inittab","shutdown","init","$etc$init.d$","telinit","systemd","systemctl","$etc$systemd$","$usr$lib$systemd$","wall","$ (root) filesystem","$var filesystem","$home filesystem","$boot filesystem","EFI System Partition (ESP)","swap space","mount points","partitions","menu.lst, grub.cfg and grub.conf","grub-install","grub-mkconfig","MBR","ldd","ldconfig","$etc$ld.so.conf","LD_LIBRARY_PATH","$etc$apt$sources.list","dpkg","dpkg-reconfigure","apt-get","apt-cache","rpm","rpm2cpio","$etc$yum.conf","$etc$yum.repos.d$","yum","zypper","Virtual machine","Linux container","Application container","Guest drivers","SSH host keys","D-Bus machine id","bash","echo","env","export","pwd","set","unset","type","which","man","uname","history",".bash_history","Quoting","bzcat","cat","cut","head","less","md5sum","nl","od","paste","sed","sha256sum","sha512sum","sort","split","tail","tr","uniq","wc","xzcat","zcat","cp","find","mkdir","mv","ls","rm","rmdir","touch","tar","cpio","dd","file","gzip","gunzip","bzip2","bunzip2","xz","unxz","file globbing","tee","xargs","&","bg","fg","jobs","kill","nohup","ps","top","free","uptime","pgrep","pkill","killall","watch","screen","tmux","nice","ps","renice","top","grep","egrep","fgrep","sed","regex(7)","vi","$, ?","h,j,k,l","i, o, a","d, p, y, dd, yy","ZZ, :w!, :q!","EDITOR","ext2$ext3$ext4","XFS","VFAT","exFAT","fdisk","gdisk","parted","mkfs","mkswap","du","df","fsck","e2fsck","mke2fs","tune2fs","xfs_repair","xfs_fsr","xfs_db","$etc$fstab","$media$","mount","umount","blkid","lsblk","chmod","umask","chown","chgrp","ln","ls")
+base = os.getcwd()
+for i in themes:
+    file_name = base + "\\"+str(i)
+    try:
+        os.mkdir(file_name)
+    except:
+        pass
